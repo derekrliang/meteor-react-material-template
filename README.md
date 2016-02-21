c80f7da7ca9e3631f1f7348749d1042a20bc2c8f
Derek Liang <frostyrusty@gmail.com>
2016-02-21 12:42:11 -0800

    checkpoint material-ui, switch to materialize css

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ Yet another meteor template.
 Currently based on the latest and greatest (experimental) version of Meteor:
 * 1.3-beta.11
 
-Objective:
+**Please note that this is using the experimental version of Meteor, bugs are to be expected.**
+
+## Objective:
 * Explore the newest version of Meteor and find bugs/defects.
 * Establish a template for new Meteor projects that hope to baseline on the latest baseline.
-* Learning ES6 and new Meteor features.
+* Learn ES6 and new Meteor features.
 
-**Please note that this is using the experimental version of Meteor, bugs are to be expected.**
+## Design Decisions/Notes:
+
+* Inline styles vs. CSS/SCSS (02/21/16)
+** Initially, the plan was to use Material-UI since it had synergy with React. After fiddling around with inline/JS injected styles, I still prefer CSS/SCSS because 1) it can be cached, 2) not JS dependent. Material-UI is nice, but I don't think we are ready to take on inline styles as a replacement for CSS just yet. I then switched to MaterializeCSS.
--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
+import FullWidthSection from './FullWidthSection.jsx';
 
 export class Footer extends React.Component {
     render() {
         return (
-            <div>
-                Footer HERE
-            </div>
+            <FullWidthSection style={styles.footer}>
+                <p style={prepareStyles(styles.p)}>
+                    {'Hand crafted with love by the engineers at '}
+                    <a style={styles.a} href="http://call-em-all.com">
+                        Call-Em-All
+                    </a>
+                    {' and our awesome '}
+                    <a
+                        style={prepareStyles(styles.a)}
+                        href="https://github.com/callemall/material-ui/graphs/contributors"
+                    >
+                        contributors
+                    </a>.
+                </p>
+                <IconButton
+                    iconStyle={styles.iconButton}
+                    iconClassName="muidocs-icon-custom-github"
+                    href="https://github.com/callemall/material-ui"
+                    linkButton={true}
+                />
+            </FullWidthSection>
         );
     }
 };
